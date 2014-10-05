73d3d5e8344b25f39bc78f0e448e763636215e98
issac <jan.issac@gmail.com>
2014-10-05 19:13:40 +0200

    Created KalmanFilter

---
+++ include/fast_filtering/filters/deterministic/kalman_filter.hpp
@@ -0,0 +1,105 @@
+/*
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2014 Max-Planck-Institute for Intelligent Systems,
+ *                     University of Southern California
+ *    Jan Issac (jan.issac@gmail.com)
+ *    Manuel Wuthrich (manuel.wuthrich@gmail.com)
+ *
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of Willow Garage, Inc. nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+/**
+ * @date 2014
+ * @author Jan Issac (jan.issac@gmail.com)
+ * @author Manuel Wuthrich (manuel.wuthrich@gmail.com)
+ * Max-Planck-Institute for Intelligent Systems, University of Southern California
+ */
+
+#ifndef FAST_FILTERING_FILTERS_DETERMINISTIC_KALMAN_FILTER_HPP
+#define FAST_FILTERING_FILTERS_DETERMINISTIC_KALMAN_FILTER_HPP
+
+#include <fast_filtering/utils/traits.hpp>
+
+#include <boost/shared_ptr.hpp>
+
+#include <fast_filtering/distributions/gaussian.hpp>
+#include <fast_filtering/models/process_models/linear_process_model.hpp>
+#include <fast_filtering/models/observation_models/linear_observation_model.hpp>
+
+namespace ff
+{
+
+template <typename ProcessModel, typename ObservationModel>
+class KalmanFilter
+{
+public:
+    typedef boost::shared_ptr<ProcessModel> ProcessModelPtr;
+    typedef boost::shared_ptr<ObservationModel> ObserationModelPtr;
+
+    typedef typename ProcessModel::State State;
+    typedef typename ProcessModel::Input Input;
+    typedef typename ProcessModel::State State;
+    typedef typename ObservationModel::Observation Observation;
+
+    typedef Gaussian<State> StateDistribution;
+
+public:
+    KalmanFilter(const ProcessModelPtr& process_model,
+                 const ProcessModelPtr& observation_model):
+        process_model_(process_model),
+        observation_model_(observation_model)
+    {
+    }
+
+    void Predict(const double delta_time,
+                 const StateDistribution& prior,
+                 StateDistribution& predicted)
+    {
+        process_model_->Condition(delta_time, prior.Mean());
+        predicted.Mean(process_model_->MapStandardGaussian);
+    }
+
+    void Update(const StateDistribution& predicted,
+                StateDistribution& posterior)
+    {
+
+    }
+
+
+protected:
+    ProcessModelPtr process_model_;
+    ObserationModelPtr observation_model_;
+};
+
+}
+
+#endif
