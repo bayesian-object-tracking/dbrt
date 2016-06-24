c507699ae61e7365820c7cee379aa6e6ac4a09e8
issac <jan.issac@gmail.com>
2016-06-24 15:34:26 +0200

    formatting

--- source/dbrt/builder/visual_tracker_factory.cpp
+++ source/dbrt/builder/visual_tracker_factory.cpp
@@ -38,7 +38,6 @@
 
 namespace dbrt
 {
-
 /**
  * \brief Create a particle filter tracking the robot joints based on depth
  *     images measurements
@@ -47,8 +46,7 @@ namespace dbrt
  * \param urdf_kinematics
  *     URDF robot kinematics
  */
-std::shared_ptr<dbrt::VisualTracker>
-create_visual_tracker(
+std::shared_ptr<dbrt::VisualTracker> create_visual_tracker(
     const std::string& prefix,
     const std::shared_ptr<KinematicsFromURDF>& urdf_kinematics,
     const std::shared_ptr<dbot::ObjectModel>& object_model,
@@ -65,58 +63,57 @@ create_visual_tracker(
     dbrt::TransitionBuilder<Tracker>::Parameters transition_parameters;
 
     // linear state transition parameters
-    transition_parameters.joint_sigmas = ri::read<std::vector<double>> (
-                                 prefix + "joint_transition/joint_sigmas", nh);
+    transition_parameters.joint_sigmas = ri::read<std::vector<double>>(
+        prefix + "joint_transition/joint_sigmas", nh);
     transition_parameters.joint_count = urdf_kinematics->num_joints();
 
     auto transition_builder =
-      std::make_shared<dbrt::TransitionBuilder<Tracker>>(transition_parameters);
+        std::make_shared<dbrt::TransitionBuilder<Tracker>>(
+            transition_parameters);
 
     /* ------------------------------ */
     /* - Observation model          - */
     /* ------------------------------ */
     dbot::RbObservationModelBuilder<State>::Parameters sensor_parameters;
 
-
-    sensor_parameters.use_gpu = ri::read<bool> (prefix + "use_gpu", nh);
+    sensor_parameters.use_gpu = ri::read<bool>(prefix + "use_gpu", nh);
 
     if (sensor_parameters.use_gpu)
     {
         sensor_parameters.sample_count =
-                ri::read<int> (prefix + "gpu/sample_count", nh);
+            ri::read<int>(prefix + "gpu/sample_count", nh);
     }
     else
     {
         sensor_parameters.sample_count =
-                ri::read<int> (prefix + "cpu/sample_count", nh);
+            ri::read<int>(prefix + "cpu/sample_count", nh);
     }
 
-    sensor_parameters.occlusion.p_occluded_visible = ri::read<double> (
-                prefix + "observation/occlusion/p_occluded_visible", nh);
-    sensor_parameters.occlusion.p_occluded_occluded = ri::read<double> (
-                prefix + "observation/occlusion/p_occluded_occluded", nh);
-    sensor_parameters.occlusion.initial_occlusion_prob = ri::read<double> (
-                prefix + "observation/occlusion/initial_occlusion_prob", nh);
+    sensor_parameters.occlusion.p_occluded_visible = ri::read<double>(
+        prefix + "observation/occlusion/p_occluded_visible", nh);
+    sensor_parameters.occlusion.p_occluded_occluded = ri::read<double>(
+        prefix + "observation/occlusion/p_occluded_occluded", nh);
+    sensor_parameters.occlusion.initial_occlusion_prob = ri::read<double>(
+        prefix + "observation/occlusion/initial_occlusion_prob", nh);
 
-
-    sensor_parameters.kinect.tail_weight = ri::read<double> (
-                prefix + "observation/kinect/tail_weight", nh);
-    sensor_parameters.kinect.model_sigma = ri::read<double> (
-                prefix + "observation/kinect/model_sigma", nh);
-    sensor_parameters.kinect.sigma_factor = ri::read<double> (
-                prefix + "observation/kinect/sigma_factor", nh);
-    sensor_parameters.delta_time = ri::read<double> (
-                prefix + "observation/delta_time", nh);
+    sensor_parameters.kinect.tail_weight =
+        ri::read<double>(prefix + "observation/kinect/tail_weight", nh);
+    sensor_parameters.kinect.model_sigma =
+        ri::read<double>(prefix + "observation/kinect/model_sigma", nh);
+    sensor_parameters.kinect.sigma_factor =
+        ri::read<double>(prefix + "observation/kinect/sigma_factor", nh);
+    sensor_parameters.delta_time =
+        ri::read<double>(prefix + "observation/delta_time", nh);
 
     // gpu only parameters
-    sensor_parameters.use_custom_shaders = ri::read<bool> (
-                prefix + "gpu/use_custom_shaders", nh);
-    sensor_parameters.vertex_shader_file = ri::read<std::string> (
-                prefix + "gpu/vertex_shader_file", nh);
-    sensor_parameters.fragment_shader_file = ri::read<std::string> (
-                prefix + "gpu/fragment_shader_file", nh);
-    sensor_parameters.geometry_shader_file = ri::read<std::string> (
-                prefix + "gpu/geometry_shader_file", nh);
+    sensor_parameters.use_custom_shaders =
+        ri::read<bool>(prefix + "gpu/use_custom_shaders", nh);
+    sensor_parameters.vertex_shader_file =
+        ri::read<std::string>(prefix + "gpu/vertex_shader_file", nh);
+    sensor_parameters.fragment_shader_file =
+        ri::read<std::string>(prefix + "gpu/fragment_shader_file", nh);
+    sensor_parameters.geometry_shader_file =
+        ri::read<std::string>(prefix + "gpu/geometry_shader_file", nh);
 
     auto sensor_builder =
         std::make_shared<dbot::RbObservationModelBuilder<State>>(
@@ -128,11 +125,10 @@ create_visual_tracker(
     dbrt::VisualTrackerBuilder<Tracker>::Parameters tracker_parameters;
     tracker_parameters.evaluation_count = sensor_parameters.sample_count;
 
-
-    tracker_parameters.moving_average_update_rate = ri::read<double>(
-                prefix + "moving_average_update_rate", nh);
-    tracker_parameters.max_kl_divergence = ri::read<double>(
-                prefix + "max_kl_divergence", nh);
+    tracker_parameters.moving_average_update_rate =
+        ri::read<double>(prefix + "moving_average_update_rate", nh);
+    tracker_parameters.max_kl_divergence =
+        ri::read<double>(prefix + "max_kl_divergence", nh);
     tracker_parameters.sampling_blocks =
         ri::read<std::vector<std::vector<int>>>(prefix + "sampling_blocks", nh);
 
